74e9542bbf0b888760e05962966d4bb22ed65042
David Welsh <welshdavmsu@gmail.com>
2025-12-19 18:39:45 -0500

    let's see if this works

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Welcome to Chirper!
+    </x-slot:title>
+    <div class="max-w-2xl">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+            
+                <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+                <p class="mt-4 text-base-content/60">
+                    The place to share your thoughts in 280 characters or less.
+                </p>
+            </div>
+        </div>
+    </div>
+</x-layout>
